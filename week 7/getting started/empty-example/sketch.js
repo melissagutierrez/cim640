1e64abadecedd0e5a6bdb250de825f6ddfef5037
melissagutierrez <mxg1519@miami.edu>
2017-10-10 15:23:38 -0400

    P5JS Exercise
    Latest

---
+++ week 7/getting started/empty-example/sketch.js
@@ -0,0 +1,33 @@
+var posX = 0;
+var posY = 0;
+
+function setup(){
+ createCanvas(1000,500);
+ background(125);
+ background(255,0,0);
+ background("purple");
+ background("#999999");
+
+ posX = 230;
+ posY = 300;
+}
+function draw(){
+    fill ("white");
+ellipse (75,75,100,100);
+ fill ("black");   
+ellipse (60,60,55,50);
+fill ("white")
+    
+ ellipse (200,75,100,100);
+    fill ("black")
+ellipse (210,55,50,50);
+
+ fill ("red");   
+
+rect (100,200,70,20);
+ fill ("white");
+stroke("cyan");
+ strokeWeight (5);
+}
+
+
